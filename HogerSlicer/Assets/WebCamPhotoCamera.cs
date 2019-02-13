181a369418f3890ddbbdf98dc32c78aa8a755cae
yuval <yuval@DESKTOP-6GJK2QI>
2019-02-14 00:02:53 +0200

    picture

---
+++ HogerSlicer/Assets/WebCamPhotoCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class WebCamPhotoCamera : MonoBehaviour
+{
+    WebCamTexture webCamTexture;
+
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        webCamTexture.Play();
+    }
+
+    void Update()
+    {
+        GetComponent<RawImage>().texture = webCamTexture;
+    }
+}
